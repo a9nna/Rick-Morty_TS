f0aa139b73648ce5811585cb92683c5dc4239d0d
Ana Coronel <a9nna.coronel@gmail.com>
2023-05-08 17:00:50 +0200

    Add styles to the character card

--- src/ts/character.ts
+++ src/ts/character.ts
@@ -1,4 +1,5 @@
 import { type CharacterStructure } from "./getCharacters/types";
+import style from "../styles/character.module.css";
 
 const createCharacterCard = ({
   name,
@@ -6,27 +7,29 @@ const createCharacterCard = ({
   origin: { name: origin },
   species,
 }: CharacterStructure) => {
-  const characterCard = `<li><article>
-    <div>
-      <span>${name}</span>
-    </div>
-    <img
-      src=${image}
-      alt=${`The face of ${name}`}
-      width=300
-      height=300
-    />
-    <div>
-      <div>
-        <span>Origin</span>
-        <span>${origin}</span>
+  const characterCard = `<li class=${style.container}>
+    <article class=${style.card}>
+      <div class=${style.card__header}>
+        <span>${name}</span>
       </div>
-      <div>
-        <span>Specie</span>
-        <span>${species}</span>
+      <img
+        src=${image}
+        alt=${`The face of ${name}`}
+        width=300
+        height=300
+      />
+      <div class=${style["card__resumed-info"]}>
+        <div class=${style.card__origin}>
+          <span>Origin</span>
+          <span>${origin}</span>
+        </div>
+        <div class=${style.card__specie}>
+          <span>Specie</span>
+          <span>${species}</span>
+        </div>
       </div>
-    </div>
-  </article></li>`;
+    </article>
+  </li>`;
 
   return characterCard;
 };
